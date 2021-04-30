357363ca7886608f1fee643cbf6aa832fb308219
Your Name <iurii.zozulynskyi@gmail.com>
2021-04-30 17:22:50 +0300

    Add db and requirements.txt

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///courses.db'
 db = SQLAlchemy(app)
 
+
 class Courses(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(200), nullable=False)
